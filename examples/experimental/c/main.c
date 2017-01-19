6dbf8dcf29b99f1882ca61087ad6f4aea710b780
dolinay <j.dolinay@email.cz>
2017-01-19 13:20:18 +0100

    experimental c-example created (reorganized).

--- examples/experimental/c/main.c
+++ examples/experimental/c/main.c
@@ -1,10 +1,16 @@
 /*
  * main.c
  *
- *
- *  Example for debugging with avr8-stub.c in plain C language,
+ * Experimental example for debugging with avr8-stub.c in plain C language,
  *  without Arduino libraries.
  *  For instructions on use please see doc\avr_debug.pdf
+ *
+ *  This project can be used for various experiments, e.g. running in linux/Windows,
+ *  filling-up program memory...
+ *  It can be easily changed for Atmega328/1280/2560:
+ *  - In project properties > AVR change target hardware (e.g. ATmega2560)
+ *  and programmer (avrdude programmer configuration).
+ *  - To debug use/create debug configuration as needed (direct serial/tcp/linux dev/windows COM,...).
  *
  *  Author: Jan Dolinay
  */
@@ -14,6 +20,8 @@
 #include <avr/pgmspace.h>
 #include "../../../avr8-stub/avr8-stub.h"	/* relative path for GDB stub valid only if this file is in the examples subfolder */
 
+/* See the fillmem.h file for option to fill up the program memory to test
+ * the debugger with larger programs. */
 #include "fillmem.h"
 
 // LED is on pin PB7 on Arduino Mega, PD5 on Arduino Uno (Arduino pin 13)
@@ -23,8 +31,9 @@
 	#define		LED_PIN		(5)
 #endif
 
-int cnt = 0;
-int function(int a);
+uint16_t cnt = 0;
+uint16_t result;
+uint16_t function(uint16_t a);
 void mydelay(void);
 
 
@@ -50,9 +59,9 @@ int main(void)
     return 0;
 }
 
-int function(int a)
+uint16_t function(uint16_t a)
 {
-	int n;
+	uint16_t n;
 	n = 2*a;
 	return n;
 }
---
+++ examples/experimental/readme.txt
@@ -0,0 +1,2 @@
+This is experimental code. 
+c - example program for testing the debugger including filling the memory and running avr-gdb from command line.

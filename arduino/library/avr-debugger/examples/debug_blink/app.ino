8119a1a6045a0864568554f946368225dc4d342f
Jan Dolinay <j.dolinay@email.cz>
2019-06-14 12:08:05 +0200

    Add example for arduino library.

---
+++ arduino/library/avr-debugger/examples/debug_blink/app.ino
@@ -0,0 +1,40 @@
+// Example program for avr-debugger library.
+// IMPORTANT: 
+// These instructions are for Visual Studio Code. For other use cases see the readme.txt.
+// To start debugging:
+// - Add "output": ".\\build" to file arduino.json (in .vscode folder)
+// - Change the COM port number to your port number in launch.json, see "miDebuggerServerAddress": "\\\\.\\COM7"
+// - Change the path to point to avr-gdb.exe in your Arduino installation, see launch.json,  "miDebuggerPath": 
+//
+// - Place breakpoint to loop, e.g. on digitalWrite - by clicking into the left margin of the file - red dot should appear
+// - Switch to debug view
+// - Click Start debugging button (Play icon) in the top
+//
+
+#include <app_api.h>
+#include <avr8-stub.h>
+
+int globalCounter = 0;
+
+void setup()
+{
+    debug_init();	// initialize the debugger
+	
+    pinMode(13, OUTPUT);
+    digitalWrite(13, HIGH);	
+}
+
+void loop()
+{
+	int localCounter = 0;
+	localCounter++;
+   
+	digitalWrite(13, HIGH);
+	delay(100);
+	digitalWrite(13, LOW);
+	delay(200);
+	
+	globalCounter++;
+	localCounter++;
+	
+}
